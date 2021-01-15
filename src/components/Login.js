d230b039295f78adf040bca4a7bdeb49d657e394
Usman Suleiman <usmansbk@gmail.com>
2021-01-15 22:36:30 +0100

    refactor: Create login component

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,25 +2,26 @@ import logo from "../assets/hands.png";
 import "../styles/login.css";
 
 export default function Login() {
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("submit");
+  };
+
   return (
     <div className="container">
       <div className="content">
-        <div className="center">
-          <h3 className="welcome-title">
-            Welcome to the Would You Rather App!
-          </h3>
+        <div className="header">
+          <h3 className="title">Welcome to the Would You Rather App!</h3>
           <h5 className="subtitle">Please sign in to continue</h5>
         </div>
+
         <img alt="logo" src={logo} className="logo" />
         <h2 className="title">Sign In</h2>
-        <form>
-          <label>
-            User:
-            <select>
-              <option value="default">Usman</option>
-            </select>
-          </label>
-          <button className="btn">SIGN IN</button>
+        <form onSubmit={onSubmit}>
+          <select>
+            <option value="default">Usman</option>
+          </select>
+          <input type="submit" value="Sign In" className="btn" />
         </form>
       </div>
     </div>
