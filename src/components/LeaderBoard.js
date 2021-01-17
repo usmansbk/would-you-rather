ccbc82f446c977b8049bc113b9af749834f326cf
Usman Suleiman <usmansbk@gmail.com>
2021-01-17 16:35:33 +0100

    refactor: User component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -3,7 +3,36 @@ import styles from "../styles/leaderboard.module.css";
 export default function Leaderboard() {
   return (
     <>
-      <h3>Leader Board</h3>
+      <User />
     </>
+  );
+}
+
+function User({ avatarURL, name }) {
+  return (
+    <div className={styles.card}>
+      <img
+        src="https://i.pravatar.cc/300"
+        alt={name}
+        className={styles.avatar}
+      />
+      <div className={styles.container}>
+        <h3>Sarah Edo</h3>
+        <div className={styles.stat}>
+          <span>Answered questions</span>
+          <span className={styles.count}>7</span>
+        </div>
+        <div className={styles.stat}>
+          <span>Created questions</span>
+          <span className={styles.count}>3</span>
+        </div>
+      </div>
+      <div className={styles.scoreContainer}>
+        <h4>Score</h4>
+        <div className={styles.score}>
+          <h3>10</h3>
+        </div>
+      </div>
+    </div>
   );
 }
