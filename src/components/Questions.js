3b801c1e5c1423a969521aa278b9ac80865a1a8f
Usman Suleiman <usmansbk@gmail.com>
2021-01-17 10:46:16 +0100

    create question component

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -1,25 +1,33 @@
+import { Link } from "react-router-dom";
+import styles from "../styles/questions.module.css";
+
 export default function Questions() {
   return (
     <div>
-      <Question />
+      <Link to="/questions/:id" className={styles.link}>
+        <Question />
+      </Link>
     </div>
   );
 }
 
 function Question() {
   return (
-    <div>
+    <div className={styles.card}>
       <div>
-        <h1>Sarah Edo asks:</h1>
+        <h4>Sarah Edo asks:</h4>
       </div>
-      <div>
-        <img src={"https://i.pravatar.cc/300"} alt={"Sarah"} />
-        <div>
-          <p>Would you rather</p>
+      <div className={styles.itemContent}>
+        <img
+          src={"https://i.pravatar.cc/300"}
+          alt={"Sarah"}
+          className={styles.avatar}
+        />
+        <div className={styles.right}>
+          <h4>Would you rather</h4>
           <p>find $50 yourself</p>
-          <p>Or</p>
+          <small className={styles.or}>Or</small>
           <p>have your best friend find $500</p>
-          <button>View Poll</button>
         </div>
       </div>
     </div>
