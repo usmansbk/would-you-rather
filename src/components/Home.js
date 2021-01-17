0cb9f469a8f19a4e4e4b4e093df89c3c522c0e7b
Usman Suleiman <usmansbk@gmail.com>
2021-01-17 09:35:40 +0100

    style: Tabs

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,22 +1,47 @@
+import clsx from "clsx";
+import { useState } from "react";
 import styles from "../styles/home.module.css";
 
+const ANSWERED = "answered";
+const UNANSWERED = "unanswered";
+
 export default function Home() {
+  const [tab, setTab] = useState(UNANSWERED);
+
+  const _goTo = (e, tab) => {
+    e.preventDefault();
+    setTab(tab);
+  };
+
   return (
     <div className={styles.container}>
-      <nav>
-        <ul className={styles.list}>
-          <li className={styles.item}>
-            <a href="#unanswered" className={styles.link}>
-              Unanswered Questions
-            </a>
-          </li>
-          <li className={styles.item}>
-            <a href="#answered" className={styles.link}>
-              Answered Questions
-            </a>
-          </li>
-        </ul>
-      </nav>
+      <div className={styles.content}>
+        <nav>
+          <ul className={styles.list}>
+            <li className={styles.item}>
+              <a
+                href="#unanswered"
+                className={clsx(
+                  styles.link,
+                  tab === UNANSWERED && styles.active
+                )}
+                onClick={(e) => _goTo(e, UNANSWERED)}
+              >
+                Unanswered Questions
+              </a>
+            </li>
+            <li className={styles.item}>
+              <a
+                href="#answered"
+                className={clsx(styles.link, tab === ANSWERED && styles.active)}
+                onClick={(e) => _goTo(e, ANSWERED)}
+              >
+                Answered Questions
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
